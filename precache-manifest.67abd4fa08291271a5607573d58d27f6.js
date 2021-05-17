ec95e9a5e8029abbd0ea9722b0940f7d36e78256
Code Sandbox <hello@codesandbox.io>
2021-05-17 20:54:51 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.67abd4fa08291271a5607573d58d27f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba9e61d067374cc643b4",
+    "url": "https://buraakkk.github.io/csb-d39b1/static/css/main.b12b4fe7.chunk.css"
+  },
+  {
+    "revision": "ba9e61d067374cc643b4",
+    "url": "https://buraakkk.github.io/csb-d39b1/static/js/main.7f71201b.chunk.js"
+  },
+  {
+    "revision": "15aa181c38251caf1d82",
+    "url": "https://buraakkk.github.io/csb-d39b1/static/js/runtime~main.3829fdd3.js"
+  },
+  {
+    "revision": "d78519df44ec2c64bd4e",
+    "url": "https://buraakkk.github.io/csb-d39b1/static/js/2.ec2d356b.chunk.js"
+  },
+  {
+    "revision": "1577cf45006d16d28d9a2dfdbce8fc30",
+    "url": "https://buraakkk.github.io/csb-d39b1/index.html"
+  }
+];
